009619bacea02521d3dbc8f2d5489c2109222572
ZS iMac <zsolis@deltacontrols.com>
2025-05-31 23:15:55 -0700

    Update base path.

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(), tailwindcss()],
-  base: "/nca-club-hub/",
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
