05683a41249a884df001501d0e54433181f4cb40
ZS iMac <zsolis@deltacontrols.com>
2025-06-01 22:55:48 -0700

    Set light as default theme

--- src/components/common/theme/ThemeContext.tsx
+++ src/components/common/theme/ThemeContext.tsx
@@ -1,13 +1,13 @@
 import { createContext } from "react";
 
-export type Theme = "dark" | "light" | "system";
+export type Theme = "dark" | "light";
 export type ThemeProviderState = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
 };
 
 const initialState: ThemeProviderState = {
-  theme: "system",
+  theme: "light",
   setTheme: () => null,
 };
 
